dd1fb22a7d786fdc14507ad2d6a753495f23cd63
DiegoNina <dninas@unsa.edu.pe>
2022-12-25 12:07:38 -0500

    Hola Mundillo

--- src/juegofp2a/JuegoFP2A.java
+++ src/juegofp2a/JuegoFP2A.java
@@ -3,5 +3,7 @@ package juegofp2a;
 public class JuegoFP2A {
     public static void main(String[] args) {
         System.out.println("Estamos Empezando");
+        
+        System.out.println("Hola mundillo");
     }
 }
